823423f85e9f92fd76ed6375a45e434c158a1a09
mxcat1 <mxcat.xxxtremxx@gmail.com>
2018-10-28 23:39:58 -0500

    Empezando nuevo curso de vuejs

---
+++ Proyecto 1 Vuejs simple/php/conexion/conexion.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MXCAT_PC
+ * Date: 28/10/2018
+ * Time: 23:38
+ */
+
+class conexion
+{
+
+}
---
+++ Proyecto 1 Vuejs simple/php/conexion/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MXCAT_PC
+ * Date: 28/10/2018
+ * Time: 23:38
+ */
